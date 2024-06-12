d298ff7d5186f2f10ce880011c794a46f97bd404
taoufik <taoufikhoucine@gmail.com>
2024-06-12 05:55:47 +0100

    section unlock

--- Nexcent/assets/js/script.js
+++ Nexcent/assets/js/script.js
@@ -3,15 +3,19 @@ document.addEventListener('DOMContentLoaded', function() {
     const homeSection = document.getElementById('home');
   
     navbarToggler.addEventListener('click', function() {
-        const currentTop = parseInt(homeSection.style.top || '95px');
-        if (currentTop === 95) {
-            homeSection.style.top = `calc(200px + var(--navbar-height))`;
+        const currentTop = parseInt(homeSection.style.top || '105px');
+        if (currentTop === 105) {
+            homeSection.style.top = `calc(250px + var(--navbar-height))`;
         } else {
-            homeSection.style.top = '95px';
+            homeSection.style.top = '105px';
         }
     });
   
     // Update CSS custom property to store the navbar height
+    const navbarHeight = document.querySelector('header').offsetHeight;
+    document.documentElement.style.setProperty('--navbar-height', `${navbarHeight}px`);
+});
+document.addEventListener('DOMContentLoaded', function() {
     const navbarHeight = document.querySelector('header').offsetHeight;
     document.documentElement.style.setProperty('--navbar-height', `${navbarHeight}px`);
 });
